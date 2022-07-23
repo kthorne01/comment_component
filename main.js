cd3557f274433a69e373b8fcff6bd92e6d530883
Kayla Thorne <kthornetdcp@gmail.com>
2022-07-23 19:55:45 -0400

    Made changes to css

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 import "./style.css";
 
 document.querySelector("#app").innerHTML = `
+  <div>
+    <section>
     <h1>We Value Your Opinion!</h1>
-    <div>
+    </section>
+    <section>
     <c-comment name="JA" email="kthornetdcp@gmail.com" comment="I am me."></c-comment>
     <c-comment name="AB" email="tthorne@yahoo.com" comment="You are you."></c-comment>
     <c-comment name="AM" email="boondocks@hotmail.com" comment="Be who you are."></c-comment>
+    </section>
   </div>
 `;
 
