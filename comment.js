9650711aa25bc96561c070145f80e7a2e1afa36b
Kayla Thorne <kthornetdcp@gmail.com>
2022-08-02 22:01:38 -0400

    Added Date & Time

--- comment.js
+++ comment.js
@@ -4,7 +4,9 @@ class Comment extends HTMLElement {
     }
     //builds the html for the component
     connectedCallback() {
-        this.innerHTML = 
+
+      const shadow = this.attachShadow({mode: "open"});
+        shadow.innerHTML = 
         `<div class="comment card">
         <h5>***********************************************</h5>
         <h3> 
@@ -16,8 +18,37 @@ class Comment extends HTMLElement {
         <h3>
         <em><strong>Your Testimony:</em></strong> ${this.getAttribute("comment")}
         </h3>
+        <h6>
+        ${this.getAttribute("day_time")} <br>
+        ${this.getAttribute("only_time")}
+        </h6>
         <h5>***********************************************</h5>
-        </div>`;
+        </div>
+        
+        <style>
+        .comment {
+          border: 1px navy solid;
+          border-radius: 5px;
+          padding: 15px;
+          width: 450px;
+          background-color: deepskyblue;
+          box-shadow: 10px 10px rgb(19, 66, 99);
+          margin-top:10px ;
+          margin-bottom:10px ;
+          margin-left: auto;
+          margin-right: auto;
+          text-align:center;
+        }
+
+        .card {
+          padding: 2em;
+          background-color: deepbluesky;
+          text-align: center;
+        }
+
+        </style>
+        
+        `;
     }
   }
 
--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import "./style.css";
 
-
 document.querySelector("#app").innerHTML = `
 <section>
 <h1>What's Your Testimony?</h1>
@@ -23,10 +22,10 @@ document.querySelector("#app").innerHTML = `
    <p id="data"></p>
   <div>
     <section id="comments">
-    <c-comment name="JA" email="kthornetdcp@gmail.com" comment="I made it through COVID."></c-comment>
-    <c-comment name="AB" email="tthorne@yahoo.com" comment="I was able to transition into tech."></c-comment>
+    <c-comment name="JA" email="kthornetdcp@gmail.com" comment="I made it through COVID." day_time="7-15-2022" only_time="4:55 AM"></c-comment>
+    <c-comment name="AB" email="tthorne@yahoo.com" comment="I was able to transition into tech." day_time="7-15-2022" only_time="4:55 AM"></c-comment>
     <c-comment name="AM" email="boondocks@hotmail.com" comment="I passed my class after praying for an increase
-    in my mental capacity."></c-comment>
+    in my mental capacity." day_time="7-15-2022" only_time="4:55 AM"></c-comment>
     </section>
   </div>
 `;
@@ -35,9 +34,9 @@ document.querySelector("#app").innerHTML = `
 //   let data = "";  let name = document.getElementById("#userName").value
 //   let email = document.getElementById("#userEmail").value
 //   let comment = document.getElementById("userComment").value
- 
+
 //  data = "User name : "+name+"<br/>User email : "+email+ "<br/>User comment : "+comment
- 
+
 //  document.getElementById("data").innerHTML = data  // display data to paragraph
 //  };
 
@@ -46,18 +45,39 @@ const addComment = (ev) => {
   const name = document.querySelector("#userName").value;
   const email = document.querySelector("#userEmail").value;
   const comment = document.querySelector("#userComment").value;
+  const currentDate = new Date();
+  const time = currentDate.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit'});
+ ;
+
+  const currentDayOfMonth = currentDate.getDate();
+  const currentMonth = currentDate.getMonth(); // Be careful! January is 0, not 1
+  const currentYear = currentDate.getFullYear();
+  
+
+  const dateString =
+    // currentMonth + 1 + "-" + currentDayOfMonth + "-" + currentYear + " "+ time;
+    currentMonth + 1 + "-" + currentDayOfMonth + "-" + currentYear;
+    
 
   console.log(name);
   console.log(email);
   console.log(comment);
+  console.log(dateString);
+  console.log(time);
 
-  //building a new tag and appending it to ........ in the DOM 
+
+  //building a new tag and appending it to ........ in the DOM
   const template = `
   <c-comment 
     name="${name}" 
     email="${email}" 
-    comment="${comment}">
+    comment="${comment}"
+    day_time="${dateString}"
+    only_time="${time}">
   </c-comment>`;
-  document.querySelector("#comments").insertAdjacentHTML("afterbegin",template);
+  document
+    .querySelector("#comments")
+    .insertAdjacentHTML("afterbegin", template);
 };
+
 document.querySelector("form").addEventListener("submit", addComment);
